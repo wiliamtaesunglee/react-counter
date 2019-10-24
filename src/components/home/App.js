bf61de19700fdb981fd28007a4491a2d2dc7f264
wil.i.am <wiliam@wiliams-MacBook-Pro.local>
2019-10-24 09:14:28 -0300

    added redux and initiated test of the counter

--- src/components/home/App.js
+++ src/components/home/App.js
@@ -1,6 +1,37 @@
 import React from "react";
 import "./App.module.css";
 
-const App = () => <p>Tá indo</p>;
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+console.assert(counter(0, { type: "INCREMENT" }) === 1);
+
+console.assert(counter(1, { type: "INCREMENT" }) === 2);
+
+console.assert(counter(2, { type: "DECREMENT" }) === 1);
+
+console.assert(counter(5, { type: "DECREMENT" }) === 4);
+
+console.assert(counter(5, { type: "SOMETHING" }) === 5);
+
+console.assert(counter(undefined, {}) === 0);
+
+const App = () => {
+  return (
+    <div>
+      <h1>0</h1>
+      <button>-</button>
+      <button>+</button>
+    </div>
+  );
+};
 
 export default App;
